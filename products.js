ac7c5479124a9d9363cce31e8804bafeab7c56b4
a-aksakal <alican_5000@hotmail.com>
2022-04-18 15:34:41 +0300

    array Methods

---
+++ products.js
@@ -0,0 +1,38 @@
+var products = [
+  {
+    productId: 1,
+    productName: "Elma",
+    price: 10,
+    quantity: 1,
+    category: {
+      categoryName: "Meyve",
+    },
+  },
+  {
+    productId: 2,
+    productName: "Armut",
+    price: 12,
+    quantity: 5,
+    category: {
+      categoryName: "Meyve",
+    },
+  },
+  {
+    productId: 3,
+    productName: "Kiraz",
+    price: 3,
+    quantity: 10,
+    category: {
+      categoryName: "Meyve",
+    },
+  },
+  {
+    productId: 3,
+    productName: "Pırasa",
+    price: 9,
+    quantity: 13,
+    category: {
+      categoryName: "Sebze",
+    },
+  },
+];
